3432f640611e3f9cb17e493c21fa63cf472cee2d
Idris Adebayo <aafcompuworks@gmail.com>
2023-03-25 00:04:22 -0400

    adding feature-Idris

---
+++ feature-Idris/src/main/java/com/ibt/idris-app/App.java
@@ -0,0 +1,13 @@
+package com.ibt.idris_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World this is Idris O Adebayo" );
+    }
+}
